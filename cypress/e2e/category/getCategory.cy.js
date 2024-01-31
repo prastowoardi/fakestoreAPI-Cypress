b5b14e2570ae4d92e64973e473006a9888daa61e
prastowoardi <prastowoardi616@gmail.com>
2024-01-31 10:23:00 +0700

    get all category

---
+++ cypress/e2e/category/getCategory.cy.js
@@ -0,0 +1,24 @@
+describe ('Get All product', () => {
+    it ('Get All', () => {
+        cy.request({
+            method: 'GET',
+            url: `/products/categories`,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            if (response.status === 200) {
+                expect(response.status).to.eq(200)
+                cy.log(JSON.stringify(response.body))
+            } else if (response.status === 400 || response.status === 401) {
+                expect(response.status).to.eq(400) || expect(response.status).to.eq(401)
+                let message = JSON.stringify(response.body.message)
+                throw new Error(`ID : ${id}, ${message}`)
+            } else if (response.status === 500) {
+                expect(response.status).to.eq(500)
+                throw new Error('Server Error')
+            }
+        })
+    })
+})
