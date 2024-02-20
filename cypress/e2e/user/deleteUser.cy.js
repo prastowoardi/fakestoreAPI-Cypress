e5e96e6f34534a12eb37fb69c5885c8e7e4b4575
prastowoardi <prastowoardi616@gmail.com>
2024-02-20 08:55:58 +0700

    edit and delete user

---
+++ cypress/e2e/user/deleteUser.cy.js
@@ -0,0 +1,30 @@
+import { generateRandomId } from "../../support/randomValue"
+
+const id = generateRandomId()
+
+describe ('Delete User', () => {
+    it ('Delete User', () => {
+        cy.request({
+            method: 'DELETE',
+            url: `/users/${id}`,
+            failOnStatusCode: false
+        }).then((response) => {
+            switch (response.status) {
+                case 200:
+                    expect(response.status).to.eq(200)
+                    cy.log(JSON.stringify(response.body))
+                    break
+                case 400:
+                case 401:
+                    expect(response.status).to.satisfy((code) => code === 400 || code === 401)
+                    let message = JSON.stringify(response.body.message)
+                    throw new Error(`${message}`)
+                case 500:
+                    expect(response.status).to.eq(500)
+                    throw new Error('Server Error')
+                default:
+                    throw new Error(`Unexpected status code: ${response.status}`)
+            }
+        })
+    })
+})
